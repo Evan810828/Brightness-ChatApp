a5e83e6a1d75c5cb7f9b75bc42442a656d22953c
Zhirun Deng <56276097+OwenKolento@users.noreply.github.com>
2023-10-15 19:45:20 -0500

    Update ChatRoom.jsx

--- src/chat/chatRoom/ChatRoom.jsx
+++ src/chat/chatRoom/ChatRoom.jsx
@@ -122,7 +122,7 @@ export default function ChatRoom(params) {
                                     <div className='hover:bg-slate-200 rounded-lg px-3 py-2 flex justify-end cursor-pointer mb-2' onClick={()=>console.log("Clicked")} >
                                         <div className='flex'>
                                             <div className='flex flex-col ml-2'>
-                                                <span>{item.id}</span>
+                                                <span className='text-right'>{item.id}</span>
                                                 <span className='text-sm font-light text-slate-500'>{item.message}</span>
                                             </div>
                                             <Avatar src={item.avatar} />
