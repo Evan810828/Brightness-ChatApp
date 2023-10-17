e14f8ef20baf4762c3fb8625b80e0f7a0942a592
Zhirun Deng <56276097+OwenKolento@users.noreply.github.com>
2023-10-17 10:33:41 -0500

    make it better

--- server/server.js
+++ server/server.js
@@ -3,19 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '..', 'build')));
-app.get("/", (req, res) => {   
-    res.sendFile(path.join(__dirname, '../client/build', "index.html")); 
-});
-
-app.get('/login', (req, res) => {
-	res.sendFile(path.join(__dirname, '../client/build', "index.html"));
-});
-app.get('/profile', (req, res) => {
-	res.sendFile(path.join(__dirname, '../client/build', "index.html"));
-});
-app.get('/social', (req, res) => {
-	res.sendFile(path.join(__dirname, '../client/build', "index.html"));
-});
-app.listen(port, () => {
-    console.log('Server is running on port: ' + port);
-});
+app.use(express.static(__dirname + '/client/build'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  });
