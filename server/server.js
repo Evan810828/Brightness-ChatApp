48232f7fe6108cdd7cc178d7b7ccef35863f3ae5
Zhirun Deng <56276097+OwenKolento@users.noreply.github.com>
2023-10-17 10:28:35 -0500

    make it better

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '..', 'build')));
-app.get("/*", (req, res) => {   
+app.get("/", (req, res) => {   
     res.sendFile(path.join(__dirname, '../client/build', "index.html")); 
 });
 
