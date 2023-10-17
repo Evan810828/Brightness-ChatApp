e6376d0ea094b2977c21c4f3a002c8b82dbcec22
Zhirun Deng <56276097+OwenKolento@users.noreply.github.com>
2023-10-17 10:18:58 -0500

    make it better

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '..', 'build')));
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     let url = path.join(__dirname, '../client/build', 'index.html');
     if (!url.startsWith('/app/')) // since we're on local windows
       url = url.substring(1);
@@ -11,13 +11,13 @@ app.get("*", (req, res) => {
   });
 
   app.get('/login', (req, res) => {
-	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+	res.sendFile(path.join(__dirname, '../client/build', "index.html"));
 });
 app.get('/profile', (req, res) => {
-	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+	res.sendFile(path.join(__dirname, '../client/build', "index.html"));
 });
 app.get('/social', (req, res) => {
-	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+	res.sendFile(path.join(__dirname, '../client/build', "index.html"));
 });
 app.listen(port, () => {
     console.log('Server is running on port: ' + port);
