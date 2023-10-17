d6dba2ff9bf498c6086fc8c27e3ae1b81e158356
Zhirun Deng <56276097+OwenKolento@users.noreply.github.com>
2023-10-17 10:26:21 -0500

    make it better

--- server/server.js
+++ server/server.js
@@ -3,14 +3,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '..', 'build')));
-app.get("/", (req, res) => {
-    let url = path.join(__dirname, '../client/build', 'index.html');
-    if (!url.startsWith('/app/')) // since we're on local windows
-      url = url.substring(1);
-    res.sendFile(url);
-  });
+app.get("/*", (req, res) => {   
+    res.sendFile(path.join(__dirname, '../client/build', "index.html")); 
+});
 
-  app.get('/login', (req, res) => {
+app.get('/login', (req, res) => {
 	res.sendFile(path.join(__dirname, '../client/build', "index.html"));
 });
 app.get('/profile', (req, res) => {
