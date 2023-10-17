1003feb16d738c894eaa1fb54f3c7f6ddef77e48
Zhirun Deng <56276097+OwenKolento@users.noreply.github.com>
2023-10-17 10:40:15 -0500

    make it better

--- server/server.js
+++ server/server.js
@@ -4,19 +4,19 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
-app.get("*", (req, res) => {   
+app.get("/", (req, res) => {   
     res.sendFile(path.join(__dirname, '../client/build', "index.html")); 
 });
 
-// app.get('/login', (req, res) => {
-// 	res.sendFile(path.join(__dirname, '../client/build', "index.html"));
-// });
-// app.get('/profile', (req, res) => {
-// 	res.sendFile(path.join(__dirname, '../client/build', "index.html"));
-// });
-// app.get('/social', (req, res) => {
-// 	res.sendFile(path.join(__dirname, '../client/build', "index.html"));
-// });
-// app.listen(port, () => {
-//     console.log('Server is running on port: ' + port);
-// });
+app.get('/login', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build', "index.html"));
+});
+app.get('/profile', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build', "index.html"));
+});
+app.get('/social', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build', "index.html"));
+});
+app.listen(port, () => {
+    console.log('Server is running on port: ' + port);
+});
