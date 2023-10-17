fbbf689bcbace7ec722921c86a054868b6f7a62d
Zhirun Deng <56276097+OwenKolento@users.noreply.github.com>
2023-10-17 10:47:26 -0500

    make it better

--- server/server.js
+++ server/server.js
@@ -1,22 +1,18 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(express.json());
+// Your static pre-build assets folder
 app.use(express.static(path.join(__dirname, '..', 'build')));
-
-app.get("/", (req, res) => {   
-    res.sendFile(path.join(__dirname, '../client/build', "index.html")); 
+// Root Redirects to the pre-build assets
+app.get('/', function(req,res){
+  res.sendFile(path.join(__dirname, '..', 'build'));
 });
-
-app.get('/login', (req, res) => {
-	res.sendFile(path.join(__dirname, '../client/build', "index.html"));
+// Any Page Redirects to the pre-build assets folder index.html that // will load the react app
+app.get('*', function(req,res){
+  res.sendFile(path.join(__dirname, '..', 'build/index.html'));
 });
-app.get('/profile', (req, res) => {
-	res.sendFile(path.join(__dirname, '../client/build', "index.html"));
-});
-app.get('/social', (req, res) => {
-	res.sendFile(path.join(__dirname, '../client/build', "index.html"));
-});
-app.listen(port, () => {
-    console.log('Server is running on port: ' + port);
-});
+app.listen(port, ()=>{
+  console.log("Server is running on port: ", port)
+})
