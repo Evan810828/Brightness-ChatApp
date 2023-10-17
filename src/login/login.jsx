c3a11c6b49fa8fec7bcbd4c0f039dcac70e1d444
Evan810828 <yw185@rice.edu>
2023-10-16 21:49:10 -0500

    cover page

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -11,28 +11,33 @@ export default function Login() {
     };
 
     return (
-        <div className="min-h-screen flex flex-col items-center bg-blue-100">
-            <Image className='mt-[15vh] mb-[2vh]' src={require('../logo.png')} />
-            <h1 className='text-4xl mb-[6vh]'>
-                Welcome to Brightness!
-            </h1>
-            <div className="px-8 pt-6 pb-6 bg-white shadow-lg rounded-lg flex flex-col items-center">
-                <h2 className="text-2xl mb-8 font-semibold self-center">Login in to your account</h2>
-                <div className='flex items-center mb-4 w-full'>
-                    <span className='mr-2 w-[80px]'>Username: </span>
-                    <Input className='!w-[200px]' value={username}></Input>
-                </div>
-                <div className='flex items-center mb-4'>
-                    <span className='mr-2 w-[80px]'>Password: </span>
-                    <Input className='!w-[200px]' value={password}></Input>
-                </div>
-                <div className='w-full flex justify-end mt-1'>
-                    <a className='text-blue-500 hover:underline cursor-pointer'>Forgot password?</a>
-                </div>
-                <div className='mt-6'>
-                    <Button type='primary' theme='solid' size='large' className='!text-base !bg-[#006DF0]' onClick={handleSubmit}>
-                        Login
-                    </Button>
+        <div className="min-h-screen flex bg-blue-100 justify-center">
+            <div className='flex items-center'>
+                <Image src={require('../cover.jpg')} />
+            </div>
+            <div className='flex flex-col items-center w-[50vw]'>
+                <Image className='mt-[15vh] mb-[2vh]' src={require('../logo.png')} />
+                <h1 className='text-4xl mb-[6vh]'>
+                    Welcome to Brightness!
+                </h1>
+                <div className="px-8 pt-6 pb-6 bg-white shadow-lg rounded-lg flex flex-col items-center">
+                    <h2 className="text-2xl mb-8 font-semibold self-center">Login in to your account</h2>
+                    <div className='flex items-center mb-4 w-full'>
+                        <span className='mr-2 w-[80px]'>Username: </span>
+                        <Input className='!w-[200px]' value={username}></Input>
+                    </div>
+                    <div className='flex items-center mb-4'>
+                        <span className='mr-2 w-[80px]'>Password: </span>
+                        <Input className='!w-[200px]' value={password}></Input>
+                    </div>
+                    <div className='w-full flex justify-end mt-1'>
+                        <a className='text-blue-500 hover:underline cursor-pointer'>Forgot password?</a>
+                    </div>
+                    <div className='mt-6'>
+                        <Button type='primary' theme='solid' size='large' className='!text-base !bg-[#006DF0]' onClick={handleSubmit}>
+                            Login
+                        </Button>
+                    </div>
                 </div>
             </div>
         </div>
