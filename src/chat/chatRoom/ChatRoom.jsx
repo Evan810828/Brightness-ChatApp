896b4e08757c1c54729bd31961a76482ae00d384
Zhirun Deng <56276097+OwenKolento@users.noreply.github.com>
2023-10-31 22:03:17 -0500

    make it better

--- src/chat/chatRoom/ChatRoom.jsx
+++ src/chat/chatRoom/ChatRoom.jsx
@@ -324,7 +324,9 @@ export default function ChatRoom(params) {
                                                 </div> :  null}
                                                 <div className='mr-2 w-max items-edn'>
                                                     <div className='ml-1 text-sm mb-1 text-right'>{item.senderName}</div>
-                                                    <div id={i+"m"} className='max-w-[300px] bg-white px-3 pt-1 pb-2 rounded-lg text-sm  w-max text-red-500 font-bold'>{replaceURLs(item.content)}</div>
+                                                    <div id={i+"m"} className='max-w-[300px] bg-white px-3 pt-1 pb-2 rounded-lg text-sm  w-max text-red-500 font-bold'>
+                                                        {replaceURLs(item.content)}
+                                                    </div>
                                                     {chatData[i].likes && chatData[i].likes.length > 0 ? 
                                                         <div className='px-2 py-2 bg-slate-100 text-sm w-max'>
                                                             <div className='flex '>
